7b06910f7d954c441b646dba926f6e562851c2a6
KimGenius <geniusk1047@naver.com>
2021-01-06 01:51:35 +0900

    Store, Item model migration

--- gorm.go
+++ gorm.go
@@ -6,10 +6,26 @@ import (
 	"gorm.io/gorm"
 )
 
+type Store struct {
+	gorm.Model
+	Name      string `json:"name"`
+	OwnerName string `json:"ownerName"`
+}
+
+type Item struct {
+	gorm.Model
+	Name  string `json:"name"`
+	Price uint   `json:"price"`
+}
+
 func main() {
 	println("gorm test start")
 	dialector := postgres.Open(fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s", "postgres", "password", "localhost", 5432, "gorm-vs-ent-gorm", "disable"))
 	db, err := gorm.Open(dialector, &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	err = db.AutoMigrate(&Store{}, &Item{})
 	if err != nil {
 		panic(err)
 	}
